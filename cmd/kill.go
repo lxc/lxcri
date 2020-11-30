44c482b40d440d50d8a91c40097d82b8c906a3b3
Ruben Jenster <r.jenster@drachenfels.de>
2020-11-30 19:55:23 +0100

    kill: Check  container state. Remove signalMap in favour of unix.SignalNum.
    Signed-off-by: Ruben Jenster <r.jenster@drachenfels.de>

--- cmd/kill.go
+++ cmd/kill.go
@@ -1,5 +1,3 @@
-// +build go1.10
-
 package main
 
 import (
@@ -24,79 +22,41 @@ var killCmd = cli.Command{
 `,
 }
 
-const sigzero = unix.Signal(0)
-
-var signalMap = map[string]unix.Signal{
-	"ABRT":   unix.SIGABRT,
-	"ALRM":   unix.SIGALRM,
-	"BUS":    unix.SIGBUS,
-	"CHLD":   unix.SIGCHLD,
-	"CLD":    unix.SIGCLD,
-	"CONT":   unix.SIGCONT,
-	"FPE":    unix.SIGFPE,
-	"HUP":    unix.SIGHUP,
-	"ILL":    unix.SIGILL,
-	"INT":    unix.SIGINT,
-	"IO":     unix.SIGIO,
-	"IOT":    unix.SIGIOT,
-	"KILL":   unix.SIGKILL,
-	"PIPE":   unix.SIGPIPE,
-	"POLL":   unix.SIGPOLL,
-	"PROF":   unix.SIGPROF,
-	"PWR":    unix.SIGPWR,
-	"QUIT":   unix.SIGQUIT,
-	"SEGV":   unix.SIGSEGV,
-	"STKFLT": unix.SIGSTKFLT,
-	"STOP":   unix.SIGSTOP,
-	"SYS":    unix.SIGSYS,
-	"TERM":   unix.SIGTERM,
-	"TRAP":   unix.SIGTRAP,
-	"TSTP":   unix.SIGTSTP,
-	"TTIN":   unix.SIGTTIN,
-	"TTOU":   unix.SIGTTOU,
-	"URG":    unix.SIGURG,
-	"USR1":   unix.SIGUSR1,
-	"USR2":   unix.SIGUSR2,
-	"VTALRM": unix.SIGVTALRM,
-	"WINCH":  unix.SIGWINCH,
-	"XCPU":   unix.SIGXCPU,
-	"XFSZ":   unix.SIGXFSZ,
-}
-
-func parseSignal(sig string) (unix.Signal, error) {
+func parseSignal(sig string) unix.Signal {
+	if sig == "" {
+		return unix.SIGTERM
+	}
 	// handle numerical signal value
 	if num, err := strconv.Atoi(sig); err == nil {
-		for _, signum := range signalMap {
-			if num == int(signum) {
-				return signum, nil
-			}
-		}
-		return sigzero, fmt.Errorf("signal %q is not supported", sig)
+		return unix.Signal(num)
 	}
 
 	// gracefully handle all string variants e.g 'sigkill|SIGKILL|kill|KILL'
-	s := strings.TrimPrefix(strings.ToUpper(sig), "SIG")
-	signum, exists := signalMap[s]
-	if !exists {
-		return sigzero, fmt.Errorf("signal %q not supported", sig)
+	s := strings.ToUpper(sig)
+	if !strings.HasPrefix(s, "SIG") {
+		s = "SIG" + s
 	}
-	return signum, nil
+	return unix.SignalNum(s)
 }
 
 func doKill(ctx *cli.Context) error {
 	sig := ctx.Args().Get(1)
-	if len(sig) == 0 {
-		return errors.New("missing signal")
+	signum := parseSignal(sig)
+	if signum == 0 {
+		return fmt.Errorf("invalid signal param %q", sig)
 	}
 
-	signum, err := parseSignal(sig)
-	if err != nil {
-		return errors.Wrap(err, "invalid signal param")
-	}
-
-	err = clxc.loadContainer()
+	err := clxc.loadContainer()
 	if err != nil {
 		return errors.Wrap(err, "failed to load container")
+	}
+
+	state, err := clxc.getContainerState()
+	if err != nil {
+		return err
+	}
+	if !(state == StateCreated || state == StateRunning) {
+		return fmt.Errorf("can only kill container in state Created|Running but was %q", state)
 	}
 
 	return clxc.killContainer(signum)
