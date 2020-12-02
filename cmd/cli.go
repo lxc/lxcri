9919a110db89476316c41336d5ed1dad5c127880
Ruben Jenster <r.jenster@drachenfels.de>
2020-12-02 12:20:02 +0100

    refactor: Rename main to cli. Move delete and start command to cli.
    Signed-off-by: Ruben Jenster <r.jenster@drachenfels.de>

--- cmd/cli.go
+++ cmd/cli.go
@@ -279,3 +279,45 @@ func loadEnvFile(envFile string) (map[string]string, error) {
 	}
 	return env, nil
 }
+
+var deleteCmd = cli.Command{
+	Name:   "delete",
+	Usage:  "deletes a container",
+	Action: doDelete,
+	ArgsUsage: `[containerID]
+
+<containerID> is the ID of the container to delete
+`,
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "force",
+			Usage: "force deletion",
+		},
+	},
+}
+
+func doDelete(ctx *cli.Context) error {
+	return clxc.Delete(ctx.Bool("force"))
+}
+
+var startCmd = cli.Command{
+	Name:   "start",
+	Usage:  "starts a container",
+	Action: doStart,
+	ArgsUsage: `[containerID]
+
+starts <containerID>
+`,
+	Flags: []cli.Flag{
+		&cli.DurationFlag{
+			Name:    "timeout",
+			Usage:   "timeout for reading from syncfifo",
+			EnvVars: []string{"CRIO_LXC_START_TIMEOUT"},
+			Value:   time.Second * 5,
+		},
+	},
+}
+
+func doStart(ctx *cli.Context) error {
+	return clxc.Start(ctx.Duration("timeout"))
+}
