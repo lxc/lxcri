cf83a48a7ecc175e5882df6af686662b41fcadf3
Ruben Jenster <r.jenster@drachenfels.de>
2021-02-19 23:34:37 +0100

    Move GIT based lxc installation function to utils.sh
    Signed-off-by: Ruben Jenster <r.jenster@drachenfels.de>

--- deployment/ubuntu-install-lxc.sh
+++ deployment/ubuntu-install-lxc.sh
@@ -4,9 +4,6 @@ set -x
 # abort if subshell command exits non-zero
 set -e
 
-# Bash auto indentation `gg=G`
-# see https://unix.stackexchange.com/questions/19945/auto-indent-format-code-for-vim
-
 # see `man 5 os-release` and http://0pointer.de/blog/projects/os-release
 . /etc/os-release
 
@@ -24,37 +21,10 @@ install_lxc_ppa() {
 	apt_install lxc
 }
 
-LXC_GIT_REPO="${LXC_GIT_REPO:-https://github.com/lxc/lxc.git}"
-LXC_GIT_VERSION="${LXC_GIT_VERSION:-master}"
-
-LXC_INSTALL_TOOLS=${LXC_INSTALL_TOOLS:-no}
-LXC_INSTALL_COMMANDS=${LXC_INSTALL_COMMANDS:-no}
-LXC_INSTALL_DOC=${LXC_INSTALL_DOC:-no}
-LXC_INSTALL_API_DOCS=${LXC_INSTALL_API_DOCS:-no}
-
 LXC_BUILD_TOOLS="build-essential libtool automake pkg-config git ca-certificates"
 LXC_BUILD_LIBS="libseccomp-dev libapparmor-dev libbtrfs-dev libdevmapper-dev libcap-dev libc6-dev libglib2.0-dev"
 LXC_BUILD_DEPS="$LXC_BUILD_TOOLS $LXC_BUILD_LIBS"
 LXC_RUNTIME_DEPS="libseccomp2 libapparmor1 libbtrfs0 libdevmapper1.02.1 libcap2"
-
-install_lxc_git() {
-	local tmpdir=/tmp/lxc
-	git clone $LXC_GIT_REPO $tmpdir
-	cd $tmpdir
-	git reset --hard $LXC_GIT_VERSION
-	./autogen.sh
-	./configure --enable-bash=no --enable-seccomp=yes \
-		--enable-capabilities=yes --enable-apparmor=yes \
-		--enable-tools=$LXC_INSTALL_TOOLS --enable-commands=$LXC_INSTALL_COMMANDS \
-		--enable-static=no --enable-examples=no \
-		--enable-doc=$LXC_INSTALL_DOC --enable-api-docs=$LXC_INSTALL_API_DOCS
-	make install
-	git describe --tags >/usr/local/lib/liblxc.version.txt
-	echo /usr/local/lib >>/etc/ld.so.conf.d/local.conf
-	ldconfig
-	cd
-	rm -rf $tmpdir
-}
 
 case $LXC_INSTALL_FROM in
 "git")
--- deployment/utils.sh
+++ deployment/utils.sh
@@ -33,3 +33,30 @@ uninstall_golang() {
 	rm -rf $(go env GOCACHE)
 	rm -rf $(go env GOROOT)
 }
+
+LXC_GIT_REPO="${LXC_GIT_REPO:-https://github.com/lxc/lxc.git}"
+LXC_GIT_VERSION="${LXC_GIT_VERSION:-master}"
+
+LXC_INSTALL_TOOLS=${LXC_INSTALL_TOOLS:-no}
+LXC_INSTALL_COMMANDS=${LXC_INSTALL_COMMANDS:-no}
+LXC_INSTALL_DOC=${LXC_INSTALL_DOC:-no}
+LXC_INSTALL_API_DOCS=${LXC_INSTALL_API_DOCS:-no}
+
+install_lxc_git() {
+	local tmpdir=/tmp/lxc
+	git clone $LXC_GIT_REPO $tmpdir
+	cd $tmpdir
+	git reset --hard $LXC_GIT_VERSION
+	./autogen.sh
+	./configure --enable-bash=no --enable-seccomp=yes \
+		--enable-capabilities=yes --enable-apparmor=yes \
+		--enable-tools=$LXC_INSTALL_TOOLS --enable-commands=$LXC_INSTALL_COMMANDS \
+		--enable-static=no --enable-examples=no \
+		--enable-doc=$LXC_INSTALL_DOC --enable-api-docs=$LXC_INSTALL_API_DOCS
+	make install
+	git describe --tags >/usr/local/lib/liblxc.version.txt
+	echo /usr/local/lib >>/etc/ld.so.conf.d/local.conf
+	ldconfig
+	cd
+	rm -rf $tmpdir
+}
