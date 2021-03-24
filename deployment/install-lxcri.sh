60fa1b2b760ed7f8ca0205baea5ae6b392e3969a
Ruben Jenster <r.jenster@drachenfels.de>
2021-03-24 08:30:18 +0100

    Small build bugfixes.
    Signed-off-by: Ruben Jenster <r.jenster@drachenfels.de>

--- deployment/install-lxcri.sh
+++ deployment/install-lxcri.sh
@@ -15,7 +15,7 @@ install_crio_lxc() {
 	git clone $LXCRI_GIT_REPO $tmpdir
 	cd $tmpdir
 	git reset --hard $LXCRI_GIT_VERSION
-	# lxc installed from source with dafault installation prefix is prefered
+	# lxc installed from source with default installation prefix is prefered
 	export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
 	make install
 	cd
