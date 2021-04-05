e41e1c977f4fbb1468cfc220178042b9b593bf7a
Ruben Jenster <r.jenster@drachenfels.de>
2021-04-05 13:20:30 +0200

    Split runtime test into several tests.
    Signed-off-by: Ruben Jenster <r.jenster@drachenfels.de>

--- runtime_test.go
+++ runtime_test.go
@@ -15,21 +15,14 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// Create a package test ? test.NewRuntime()
-
-var tmpDir string
-
-func init() {
-	// /tmp has permissions 1777, never  use it as runtime / rootfs parent
-	if os.Getuid() != 0 {
-		tmpDir = os.Getenv("HOME")
-	}
+func mkdirTemp() (string, error) {
+	// /tmp has permissions 1777
+	// it should never be used as runtime or rootfs parent
+	return os.MkdirTemp(os.Getenv("HOME"), "lxcri-test")
 }
 
 func newRuntime(t *testing.T) *Runtime {
-	//wd, err := os.Getwd()
-	//require.NoError(t, err)
-	runtimeRoot, err := os.MkdirTemp(tmpDir, "lxcri-test")
+	runtimeRoot, err := mkdirTemp()
 	require.NoError(t, err)
 	t.Logf("runtime root: %s", runtimeRoot)
 
@@ -47,13 +40,11 @@ func newRuntime(t *testing.T) *Runtime {
 }
 
 func newConfig(t *testing.T, cmd string, args ...string) *ContainerConfig {
-	//wd, err := os.Getwd()
-	//require.NoError(t, err)
-	rootfs, err := os.MkdirTemp(tmpDir, "lxcri-test")
+	rootfs, err := mkdirTemp()
 	require.NoError(t, err)
 	t.Logf("container rootfs: %s", rootfs)
 
-	// copy binary to rootfs
+	// copy test binary to rootfs
 	err = exec.Command("cp", cmd, rootfs).Run()
 	require.NoError(t, err)
 
@@ -64,66 +55,18 @@ func newConfig(t *testing.T, cmd string, args ...string) *ContainerConfig {
 	cfg.LogFile = "/dev/stderr"
 	cfg.LogLevel = "trace"
 
-	if os.Getuid() != 0 {
-
-		// container user ID 0 is mapped to user creating the container
-		// --> file permissions in /.lxcri could be 0600 / 0400
-
-		// get UID/GID mapping from /etc/subgid /etc/subuid
-
-		/*
-			cfg.Linux.UIDMappings = []specs.LinuxIDMapping{
-				specs.LinuxIDMapping{ContainerID: 0, HostID: uint32(os.Getuid()), Size: 1},
-				specs.LinuxIDMapping{ContainerID: 1, HostID: 20000, Size: 65536},
-			}
-			cfg.Linux.GIDMappings = []specs.LinuxIDMapping{
-				specs.LinuxIDMapping{ContainerID: 0, HostID: uint32(os.Getgid()), Size: 1},
-				specs.LinuxIDMapping{ContainerID: 1, HostID: 20000, Size: 65536},
-			}
-		*/
-
-		// The container UID must have full access to the rootfs.
-		// If we the container UID (0) / or GID are not mapped to the owner (creator) of the rootfs
-		// it must be granted o+rwx.
-		// MkdirTemp creates with 0700
-
-		err = unix.Chmod(rootfs, 0777)
-		require.NoError(t, err)
-
-		// Using 0755 is required if container UID is not
-		// the owner of runtimeRoot
-
-		//err = unix.Chmod(runtimeRoot, 0755)
-		//require.NoError(t, err)
-
-		cfg.Linux.UIDMappings = []specs.LinuxIDMapping{
-			specs.LinuxIDMapping{ContainerID: 0, HostID: 20000, Size: 65536},
-		}
-		cfg.Linux.GIDMappings = []specs.LinuxIDMapping{
-			specs.LinuxIDMapping{ContainerID: 0, HostID: 20000, Size: 65536},
-		}
-
-	}
-
-	/*
-		cgroupns := specs.LinuxNamespace{
-			Type: specs.CgroupNamespace,
-		}
-		cfg.Linux.Namespaces = append(cfg.Linux.Namespaces, cgroupns)
-	*/
-
 	return &cfg
 }
 
-func TestRuntimeNamespaceCheck(t *testing.T) {
+func TestEmptyNamespaces(t *testing.T) {
 	rt := newRuntime(t)
 	defer os.RemoveAll(rt.Root)
 
 	cfg := newConfig(t, "lxcri-test")
 	defer os.RemoveAll(cfg.Root.Path)
 
-	// Clearing all namespaces should not work.
-	// At least PID and MOUNT must not be shared with the host.
+	// Clearing all namespaces should not work,
+	// since the mount namespace must never be shared with the host.
 	cfg.Linux.Namespaces = cfg.Linux.Namespaces[0:0]
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
@@ -144,13 +87,83 @@ func TestRuntimeNamespaceCheck(t *testing.T) {
 	require.Nil(t, c)
 }
 
-func TestRuntimeKill(t *testing.T) {
+func TestRuntimePrivileged(t *testing.T) {
+	if os.Getuid() != 0 {
+		t.Skipf("This tests only runs as root")
+	}
+
 	rt := newRuntime(t)
 	defer os.RemoveAll(rt.Root)
 
 	cfg := newConfig(t, "lxcri-test")
 	defer os.RemoveAll(cfg.Root.Path)
 
+	testRuntime(t, rt, cfg)
+}
+
+// The following tests require the following setup:
+// TODO load UID/GID mappings from /etc/subgid /etc/subuid
+
+// sudo chown -R $(whoami):$(whoami) /sys/fs/cgroup$(cat /proc/self/cgroup  | grep '^0:' | cut -d: -f3)
+/*
+[ruben@k8s-cluster8-controller lxcri]$ cat /etc/subgid
+ruben:1000:1
+ruben:20000:65536
+[ruben@k8s-cluster8-controller lxcri]$ cat /etc/subuid
+ruben:1000:1
+ruben:20000:65536
+*/
+func TestRuntimeUnprivileged(t *testing.T) {
+
+	rt := newRuntime(t)
+	defer os.RemoveAll(rt.Root)
+
+	cfg := newConfig(t, "lxcri-test")
+	defer os.RemoveAll(cfg.Root.Path)
+
+	// The container UID must have full access to the rootfs.
+	// MkdirTemp sets directory permissions to 0700.
+	// If we the container UID (0) / or GID are not mapped to the owner (creator) of the rootfs,
+	// then the rootfs and runtime directory permissions must be expanded.
+
+	err := unix.Chmod(cfg.Root.Path, 0777)
+	require.NoError(t, err)
+
+	err = unix.Chmod(rt.Root, 0755)
+	require.NoError(t, err)
+
+	cfg.Linux.UIDMappings = []specs.LinuxIDMapping{
+		specs.LinuxIDMapping{ContainerID: 0, HostID: 20000, Size: 65536},
+	}
+	cfg.Linux.GIDMappings = []specs.LinuxIDMapping{
+		specs.LinuxIDMapping{ContainerID: 0, HostID: 20000, Size: 65536},
+	}
+
+	testRuntime(t, rt, cfg)
+}
+
+func TestRuntimeUnprivileged2(t *testing.T) {
+	rt := newRuntime(t)
+	defer os.RemoveAll(rt.Root)
+
+	cfg := newConfig(t, "lxcri-test")
+	defer os.RemoveAll(cfg.Root.Path)
+
+	if os.Getuid() != 0 {
+		cfg.Linux.UIDMappings = []specs.LinuxIDMapping{
+			specs.LinuxIDMapping{ContainerID: 0, HostID: uint32(os.Getuid()), Size: 1},
+			specs.LinuxIDMapping{ContainerID: 1, HostID: 20000, Size: 65536},
+		}
+		cfg.Linux.GIDMappings = []specs.LinuxIDMapping{
+			specs.LinuxIDMapping{ContainerID: 0, HostID: uint32(os.Getgid()), Size: 1},
+			specs.LinuxIDMapping{ContainerID: 1, HostID: 20000, Size: 65536},
+		}
+	}
+
+	testRuntime(t, rt, cfg)
+}
+
+func testRuntime(t *testing.T, rt *Runtime, cfg *ContainerConfig) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
 
@@ -203,16 +216,6 @@ func TestRuntimeKill(t *testing.T) {
 
 	err = c.Release()
 	require.NoError(t, err)
-
-	/*
-		p, err := os.FindProcess(c.Pid)
-		require.NoError(t, err)
-		pstate, err := p.Wait()
-		require.NoError(t, err)
-		// The exit code should be non-zero because the process was terminated
-		// by a signal
-		fmt.Printf("monitor process exited %T: %s code:%d\n", pstate, pstate, pstate.ExitCode())
-	*/
 
 	// manpage for 'wait4' suggests to use waitpid or waitid instead, but
 	// golang/x/sys/unix only implements 'Wait4'. See https://github.com/golang/go/issues/9176
