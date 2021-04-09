6119e3097d95e865e8bf3267ef7aff69127359ab
Ruben Jenster <r.jenster@drachenfels.de>
2021-04-09 12:11:01 +0200

    Introduce spec_utils.go
    Collect helper functions that operate on specs.Spec
    without internal knowledge within an separate file.
    
    Signed-off-by: Ruben Jenster <r.jenster@drachenfels.de>

--- container.go
+++ container.go
@@ -73,27 +73,6 @@ type Container struct {
 	runtimeDir string
 }
 
-// unmapContainerID returns the (user/group) ID to which the given
-// ID is mapped to by the given idmaps.
-// The returned id will be equal to the given id
-// if it is not mapped by the given idmaps.
-func unmapContainerID(id uint32, idmaps []specs.LinuxIDMapping) uint32 {
-	for _, idmap := range idmaps {
-		if idmap.Size < 1 {
-			continue
-		}
-		maxID := idmap.ContainerID + idmap.Size - 1
-		// check if c.Process.UID is contained in the mapping
-		if (id >= idmap.ContainerID) && (id <= maxID) {
-			offset := id - idmap.ContainerID
-			hostid := idmap.HostID + offset
-			return hostid
-		}
-	}
-	// uid is not mapped
-	return id
-}
-
 func (c *Container) create() error {
 	if err := os.MkdirAll(c.runtimeDir, 0777); err != nil {
 		return fmt.Errorf("failed to create container dir: %w", err)
